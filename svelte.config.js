c55f00f9163bb530b2b9ac6768cf3ad6bda30225
endigo9740 <gundamx9740@gmail.com>
2022-06-15 17:06:23 -0500

    Updated github pages config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -21,7 +23,12 @@ const config = {
 			fallback: null,
 			precompress: false
 		}),
-		prerender: { default: true }
+		paths: {
+			base: dev ? '' : '/elden-ring-planner',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 	}
 };
 
