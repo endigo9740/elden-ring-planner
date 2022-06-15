7d670868da2a8287359778c8c37275c2036da782
endigo9740 <gundamx9740@gmail.com>
2022-06-15 16:43:15 -0500

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter({
+			// default options are shown
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: null,
+			precompress: false
 		})
 	}
 };
