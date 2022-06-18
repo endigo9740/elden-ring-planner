72929969879bfbb4193e5664f97e1d94fef2a80b
x753 <PrimaryInfinity@gmail.com>
2022-06-18 14:07:26 -0400

    Middle click to open Wiki page
    Middle clicking a cell should open the wiki page for the item, if possible.

--- src/lib/Item.svelte
+++ src/lib/Item.svelte
@@ -7,12 +7,14 @@
     export let label: string = 'Item';
 
     function open(): void { menu.set({source, slot, label}); }
+    function wiki(): void { if(e.button != 1) return; e.preventDefault(); e.stopPropagation(); window.open("https://eldenring.wiki.fextralife.com/" + e.target.querySelector('.w-full').title.replace(' ', '+')); }
 </script>
 
 {#if equipment}
 <div
     class="cell cell-hover aspect-square flex justify-center items-center"
     on:click={open}
+    on:auxclick={wiki}
 >
     {#if $equipment[slot]}
     <img src="{baseUrl}/{$equipment[slot].path}" class="w-full" title={$equipment[slot].name} alt={$equipment[slot].name}>
