f994f1103ad826224d8a28b479e750412ae47ba1
endigo9740 <gundamx9740@gmail.com>
2022-08-28 15:27:49 -0500

    Revert to auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,5 @@
-// import adapter from "@sveltejs/adapter-auto";
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
-
-// const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,20 +12,7 @@ const config = {
   ],
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		}),
-		// paths: {
-		// 	base: dev ? '' : '/elden-ring-planner',
-		// },
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal',
+    adapter: adapter(),
   },
 };
 
